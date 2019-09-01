a86aa7f88e9ac84ec8438d6f34afe3e3cbeb1797
Ioana Andreea <ioan0579@stud.kea.dk>
2019-09-01 20:43:41 +0200

    Add js for typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,22 @@
-"use strict";
+var i = 1;
+var message = document.getElementById("typewriter").innerText;
+
+function func() {
+  if (i <= message.length) {
+    var txt = message.substring(0, i);
+    document.getElementById("typewriter").innerHTML = txt;
+
+    if (i % 2 === 0) {
+      document.getElementById("typewriter").style.color = "blue";
+    } else {
+      document.getElementById("typewriter").style.color = "red";
+    }
+    setTimeout("func()", 200);
+    i++;
+  } else {
+    i = 1;
+    document.getElementById("typewriter").innerHTML = "";
+    setTimeout("func()", 1000);
+  }
+}
+func();
